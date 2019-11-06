248fcfa0dd855aa5358b7074f00bbc1911c3a980
J-Anne Carlson <carlsonjc3@J-Annes-MacBook-Pro.local>
2019-11-06 13:42:26 -0800

    merge changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,20 +9,6 @@ module.exports = function (app) {
     })
 }
 
-https://www.googleapis.com/youtube/v3/search?key=AIzaSyDi436SOwga8iFLuNLqZ66aXOGsDMjiVLw&part=snippet&q=coding bootcamp
-
-// /**API ROUTES TEMPLATE**/
-// const axios = require("axios");
-// const router = require("express").Router();
-
-// app.get("/__NAMEHERE__", (req, res) => {
-//   axios
-//     .get("___URL_ROUTE__HERE___", { params: req.query })
-//     .then(({ data: { results } }) => res.json(results))
-//     .catch(err => res.status(422).json(err));
-// });
-/******************************* */
-
 app.get("/api/jobPostings", (req, res)=>{
     axios
     .get("https://authenticjobs.com/api/?api_key=" + process.env.AUTHENTIC_JOBS + "&method=aj.jobs.search&keywords=php,mysql&format=json")
